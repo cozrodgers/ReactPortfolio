9197268bcc7031402a776a2183a58da3f8551aea
Corey <coreyrodgers95@gmail.com>
2019-03-25 19:54:35 +0000

    add foundation files to portfolio, add react-router and react-mdl

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Header, Drawer, Navigation, Content } from 'react-mdl';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
-          <p>Hello this is the initial commit of the app</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="demo-big-content">
+      <Layout>
+          <Header title="Corey Rodgers" scroll>
+              <Navigation>
+                  <a href="#">Home</a>
+                  <a href="#">Projects</a>
+                  <a href="#">Resume</a>
+                  <a href="#">About</a>
+                  <a href="#">Contact</a>
+              </Navigation>
+          </Header>
+          <Drawer title="Corey Rodgers">
+              <Navigation>
+              <a href="#">Home</a>
+                  <a href="#">Projects</a>
+                  <a href="#">Resume</a>
+                  <a href="#">About</a>
+                  <a href="#">Contact</a>
+              </Navigation>
+          </Drawer>
+          <Content>
+              <div className="page-content" />
+          </Content>
+      </Layout>
+  </div>
     );
   }
 }
